6eaf7fa2c069c426836bf28c70fe5c5dd5c4cc1c
Susilo Hartomo <60213970+susilo-hartomo@users.noreply.github.com>
2022-09-19 00:34:13 +0700

    update about us page, create section our mission

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -2,5 +2,8 @@ import humbergerIcn from './humberger_ic.svg';
 import cartIcn from './cart_ic.svg';
 import userIcn from './user_ic.svg';
 import closeIcn from './close_icn.svg';
+import callRecivedIc from './call_received_ic.svg';
+import messageIc from './messages_icn.svg';
+import awardIc from './award_ic.svg';
 
-export { humbergerIcn, cartIcn, userIcn, closeIcn };
+export { humbergerIcn, cartIcn, userIcn, closeIcn, callRecivedIc, messageIc, awardIc };
