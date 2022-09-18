65a4fda5650e6683fd5ccf399d79cfd1bcc52677
Susilo Hartomo <60213970+susilo-hartomo@users.noreply.github.com>
2022-09-18 14:30:36 +0700

    done topbar

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -1,5 +1,6 @@
 import humbergerIcn from './humberger_ic.svg';
 import cartIcn from './cart_ic.svg';
 import userIcn from './user_ic.svg';
+import closeIcn from './close_icn.svg';
 
-export { humbergerIcn, cartIcn, userIcn };
+export { humbergerIcn, cartIcn, userIcn, closeIcn };
