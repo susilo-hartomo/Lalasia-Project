66ef0ec9fcf9f454176ff17446c11a6e0661d062
Susilo Hartomo <60213970+susilo-hartomo@users.noreply.github.com>
2022-09-19 09:40:44 +0700

    update about us page, create section our mission

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -21,40 +21,44 @@ function aboutUs() {
 
 	const renderOurMission = () => {
 		return (
-			<div className='lg:flex lg:my-[90px] lg:gap-20 xl:gap-24  px-4 lg:px-8 xl:px-0'>
-				<div className='lg:w-1/2'>
-					<h5 className='text-secondary-1 mb-4'>Our Mission</h5>
-					<h2 className='heading pr-6'>Our team dedicated to help find smart home product</h2>
-					<div className='flex gap-12 lg:mt-12 mt-8'>
-						<CountingDesc total={20} prefix='+' desc='Years Experience' />
-						<CountingDesc total={483} desc='Happy Client' />
-						<CountingDesc total={150} prefix='+' desc='Project Finished' />
+			<div className='lg:my-[90px] px-4 lg:px-8 xl:px-0'>
+				<h5 className='text-secondary-1 mb-4'>Our Mission</h5>
+				<div className='lg:flex  lg:gap-20 xl:gap-24 '>
+					<div className='lg:w-1/2'>
+						<h2 className='heading pr-6'>Our team dedicated to help find smart home product</h2>
+						<div className='flex gap-12 lg:mt-12 mt-8'>
+							<CountingDesc total={20} prefix='+' desc='Years Experience' />
+							<CountingDesc total={483} desc='Happy Client' />
+							<CountingDesc total={150} prefix='+' desc='Project Finished' />
+						</div>
 					</div>
-				</div>
-				<div className='lg:w-1/2 mt-8 lg:mt-0'>
-					<MissionDesc
-						containerStyle='mb-10'
-						icon={callRecivedIc}
-						alt='mission priority'
-						title={'24/7 Supports'}
-						desc={'24/7 support means a support service that is provided 24 hours a day and 7 days a week.'}
-					/>
-					<MissionDesc
-						containerStyle='mb-10'
-						icon={messageIc}
-						alt='mission priority'
-						title={'Free Consultation'}
-						desc={`A free consultation is a one-on-one interaction or conversation given freely to share one's thoughts and discuss possible`}
-					/>
-					<MissionDesc
-						containerStyle='mb-10'
-						icon={awardIc}
-						alt='mission priority'
-						title={'Overall Guarantee'}
-						desc={
-							'The comprehensive guarantee is required for import, warehousing, transit, processing and specific use.'
-						}
-					/>
+					<div className='lg:w-1/2 mt-8 lg:mt-0'>
+						<MissionDesc
+							containerStyle='mb-10'
+							icon={callRecivedIc}
+							alt='mission priority'
+							title={'24/7 Supports'}
+							desc={
+								'24/7 support means a support service that is provided 24 hours a day and 7 days a week.'
+							}
+						/>
+						<MissionDesc
+							containerStyle='mb-10'
+							icon={messageIc}
+							alt='mission priority'
+							title={'Free Consultation'}
+							desc={`A free consultation is a one-on-one interaction or conversation given freely to share one's thoughts and discuss possible`}
+						/>
+						<MissionDesc
+							containerStyle='mb-10'
+							icon={awardIc}
+							alt='mission priority'
+							title={'Overall Guarantee'}
+							desc={
+								'The comprehensive guarantee is required for import, warehousing, transit, processing and specific use.'
+							}
+						/>
+					</div>
 				</div>
 			</div>
 		);
