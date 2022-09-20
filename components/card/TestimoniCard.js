ab9c8a199dbdf99a06a2aba63d5fdd32dd59ea0c
Susilo Hartomo <60213970+susilo-hartomo@users.noreply.github.com>
2022-09-20 19:06:17 +0700

    update home section

--- components/card/TestimoniCard.js
+++ components/card/TestimoniCard.js
@@ -1,19 +1,19 @@
 import React, { useRef } from 'react';
 import Image from 'next/image';
 
-function testimoniCard({ name, quote, rate, img, iconQuote, iconStar }) {
-    return (
-        <div className='w-[545px] h-[256px]' style={{ boxShadow: '0px 4px 100px rgba(175, 173, 181, 0.1)' }}>
-            <div>
-                <Image src={iconQuote} alt={'iconQuote'} />
-            </div>
-            <p>{quote}</p>
-            <Image src={img} alt={acountImage} />
-            <h4>{name}</h4>
-            <Image src={iconStar} alt={'star'} />
-            <h5>{rate}</h5>
-        </div>
-    );
+function TestimoniCard({ name, quote, rate, img, iconQuote, iconStar }) {
+	return (
+		<div className='w-[545px] h-[256px]' style={{ boxShadow: '0px 4px 100px rgba(175, 173, 181, 0.1)' }}>
+			<div>
+				<Image src={iconQuote} alt={'iconQuote'} />
+			</div>
+			<p>{quote}</p>
+			<Image src={img} alt={'acountImage'} />
+			<h4>{name}</h4>
+			<Image src={iconStar} alt={'star'} />
+			<h5>{rate}</h5>
+		</div>
+	);
 }
 
-export default testimoniCard;
+export default TestimoniCard;
