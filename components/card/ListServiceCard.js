4ce2fb56c32810ca1deaad67c2416d8e48870470
akbarimawan <akbarimawan18@gmail.com>
2022-09-20 17:38:47 +0700

    update style class in service page

--- components/card/ListServiceCard.js
+++ components/card/ListServiceCard.js
@@ -3,9 +3,9 @@ import React from "react";
 function ListServiceCard({ number, title, desc }) {
   return (
     <div className="text-left lg:w-[32%]">
-      <h1 className="w-9 h-11 text-4xl lg:text-5xl font-bold text-primary-1">
+      <h2 className="heading w-9 text-primary-1">
         {number}
-      </h1>
+      </h2>
       <h5 className="mt-5 font-bold text-lg text-title-1">{title}</h5>
       <p className="tracking-wide text-paragraph-1 mt-2">{desc}</p>
     </div>
--- components/card/PortofolioServiceCard.js
+++ components/card/PortofolioServiceCard.js
@@ -2,55 +2,31 @@ import React, { useRef } from "react";
 import Image from "next/image";
 
 const PortofolioServiceImg = ({ img, title, desc, idx }) => {
-  if (idx === 0) {
-    return (
-      <div className="w-full p-2 h-[409px] relative lg:w-1/3 lg:h-full">
-        <Image
-          src={img}
-          alt={"portfolio" + idx}
-          layout="fill"
-          objectFit="cover"
-        />
-        <div className="w-full absolute bottom-0 px-7 mb-6 lg:mb-8">
-          <div className="w-full">
-            <h5 className="text-light-white font-semibold text-md lg:text-2xl">
-              {title}
-            </h5>
-            <p className="py-2 text-light-white opacity-60 text-sm lg:text-md lg:mb-3 tracking-wider truncate whitespace-nowrap overflow-hidden">
-              {desc}
-            </p>
-            <p className="text-light-white opacity-90 text-[15px] tracking-wider cursor-pointer">
-              See Detail
-            </p>
-          </div>
+  const style = idx === 0 ? "lg:w-1/3 lg:h-full" : "lg:w-[65%] lg:h-[306px]";
+  return (
+    <div className={`w-full p-2 h-[409px] relative ${style}`}>
+      <Image
+        src={img}
+        alt={"portfolio" + idx}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="w-full absolute bottom-0 px-7 mb-6 lg:mb-8">
+        <div className="w-full">
+          <h3 className="text-light-white heading">
+            {title}
+          </h3>
+          <p className="py-2 text-light-white paragraph tracking-wider truncate whitespace-nowrap">
+            {desc}
+          </p>
+          <p className="text-light-white paragraph cursor-pointer">
+            See Detail
+          </p>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="w-full p-2 h-[409px] relative lg:w-[65%] lg:h-[306px]">
-        <Image
-          src={img}
-          alt={"portfolio" + idx}
-          layout="fill"
-          objectFit="cover"
-        />
-        <div className="w-full absolute bottom-0 px-7 mb-6 lg:mb-8">
-          <div className="w-full">
-            <h5 className="text-light-white font-semibold text-md lg:text-2xl">
-              {title}
-            </h5>
-            <p className="py-2 text-light-white opacity-60 text-sm lg:text-md lg:mb-3 tracking-wider truncate whitespace-nowrap overflow-hidden">
-              {desc}
-            </p>
-            <p className="text-light-white opacity-90 text-[15px] tracking-wider cursor-pointer">
-              See Detail
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
+
 };
 
 export default PortofolioServiceImg;
