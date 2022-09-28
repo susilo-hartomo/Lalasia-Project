3c8f9329e09529aa92ec11d1683dfcf55c4b5afc
Susilo Hartomo <60213970+susilo-hartomo@users.noreply.github.com>
2022-09-28 10:32:52 +0700

    update css in article

--- components/card/ArticleCardTwo.tsx
+++ components/card/ArticleCardTwo.tsx
@@ -20,17 +20,18 @@ export default function ArticleCard(article: ArticleTypes) {
         <p className="paragraph text-paragraph-1">{article.category}</p>
         <h3 className="heading text-title-1 my-3">{article.title}</h3>
         <p className="paragraph text-paragraph-1">{article.summary}</p>
-        <div className="flex lg:gap-6 justify-between lg:justify-start mt-4">
-          <div className="flex">
-            <Image width={28} height={28} src={article.avatar} />
-            <p className="font-bold text-sm leading-4 self-center ml-2">
-              {'By ' + article.author}
+        <div className="flex items-center mt-4">
+            <div className='flex mr-6 items-center'>
+                <Image width={28} height={28} src={article.avatar}/>
+                <p className="font-extrabold text-sm ml-3" style={{fontFamily: 'EudoxusSans-Bold'}}>
+                    {'By ' + article.author}
+                </p>
+            </div>
+            <p className="text-paragraph-1 text-sm">
+                {dateToFormatted(article.date)}
             </p>
-          </div>
-          <p className="text-paragraph-1 text-sm leading-4 self-center">
-            {dateToFormatted(article.date)}
-          </p>
         </div>
+
       </div>
     </div>
   )
